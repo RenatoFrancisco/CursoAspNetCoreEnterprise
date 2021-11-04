c2fa4b3fdf731cc978c11e9236468151c5dce2d6
RenatoFrancisco <renatofrancisco.s@gmail.com>
2021-11-03 22:40:43 -0300

    Registro e login de usuario

---
+++ src/Services/Identidade/Controllers/AuthController.cs
@@ -0,0 +1,58 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NSE.Identidade.API.Models;
+
+namespace Identidade.Controllers
+{
+    [Route("api/identidade")]
+    public class AuthController : Controller
+    {
+        private readonly SignInManager<IdentityUser> _siginINManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AuthController(SignInManager<IdentityUser> siginINManager, 
+                              UserManager<IdentityUser> userManager)
+        {
+            _siginINManager = siginINManager;
+            _userManager = userManager;
+        }
+
+        [HttpPost("nova-conta")]
+        public async Task<ActionResult> Registrar(UsuarioRegistro usuarioRegistro)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            var user = new IdentityUser
+            {
+                UserName = usuarioRegistro.Email,
+                Email = usuarioRegistro.Email,
+                EmailConfirmed = true
+            };
+
+            var result = await _userManager.CreateAsync(user, usuarioRegistro.Senha);   
+            if (result.Succeeded)
+            {
+                await _siginINManager.SignInAsync(user, isPersistent: false);
+                return Ok();
+            }
+
+            return BadRequest();
+        }
+
+        [HttpPost("Autenticar")]
+        public async Task<ActionResult> Login(UsuarioLogin usuarioLogin)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            var result = await _siginINManager.PasswordSignInAsync(usuarioLogin.Email, 
+                                                                   usuarioLogin.Senha,
+                                                                   isPersistent: false,
+                                                                   lockoutOnFailure: true);
+
+            if (result.Succeeded) return Ok();
+
+            return BadRequest();
+        }
+    }
+}
