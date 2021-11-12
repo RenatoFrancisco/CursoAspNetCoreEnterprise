29201f77cb7162ad589c60f46d7fdd095c270c82
RenatoFrancisco <renatofrancisco.s@gmail.com>
2021-11-12 19:39:35 -0300

    Models, Views e Controllers de Login

--- src/Web/MVC/Controllers/IdentidadeController.cs
+++ src/Web/MVC/Controllers/IdentidadeController.cs
@@ -1,9 +1,47 @@
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using NSE.WebApp.MVC.Models;
 
 namespace NSE.WebApp.MVC.Controllers
 {
     public class IdentidadeController : Controller
     {
-        
+        [HttpGet("nova-conta")]
+        public IActionResult Registro()
+        {
+            return View();
+        }
+
+        [HttpPost("nova-conta")]
+        public async Task<ActionResult> Registro(UsuarioRegistro usuarioRegistro)
+        {
+            if (!ModelState.IsValid) return View(usuarioRegistro);
+
+            if (false) return View(usuarioRegistro);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet("login")]
+        public async Task<IActionResult> Login()
+        {
+            return View();
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UsuarioLogin usuarioLogin)
+        {
+            if (!ModelState.IsValid) return View(usuarioLogin);
+
+            if (false) return View(usuarioLogin);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet("sair")]
+        public async Task<IActionResult> Logout()
+        {
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
