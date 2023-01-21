0dca9fed9bc3562f8b093bd7576746c610d6dd9a
renatofrancisco <renatofrancisco.s@gmail.com>
2023-01-20 23:21:30 -0300

    Comportamentos básico da API Catalogo

--- src/services/NSE.Catalogo.API/Program.cs
+++ src/services/NSE.Catalogo.API/Program.cs
@@ -1,13 +1,35 @@
-using NSE.Catalogo.API.Data.Repositories;
-
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json", true, true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true)
+    .AddEnvironmentVariables();
 
 builder.Services.AddDbContext<CatalogContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("LocaDbConnection")));
 
 builder.Services.AddControllers();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("Total", builder =>
+        builder
+            .AllowAnyOrigin()
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+});
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = $"NerdStore Enteprise Catalog API - {builder.Environment.EnvironmentName}",
+        Description = "This API makes parts of the Course ASP.NET Core Enterprise Applications"
+    });
+});
 
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<CatalogContext>();
@@ -24,6 +46,14 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+app.UseCors();
+
 app.MapControllers();
+
+app.MapGet("catalog/products", (IProductRepository productRepository) =>
+    productRepository.GetAllAsync());
+
+app.MapGet("catalog/products/{id:guid}", (IProductRepository productRepository, Guid id) =>
+    productRepository.GetAsync(id));
 
 app.Run();
--- src/services/NSE.Catalogo.API/_usings.cs
+++ src/services/NSE.Catalogo.API/_usings.cs
@@ -1,5 +1,7 @@
 ﻿global using Microsoft.EntityFrameworkCore.Metadata.Builders;
+global using NSE.Catalogo.API.Data.Repositories;
 global using Microsoft.EntityFrameworkCore;
+global using Microsoft.OpenApi.Models;
 global using NSE.Catalogo.API.Models;
 global using NSE.Core.DomainObjects;
 global using NSE.Catalogo.API.Data;
