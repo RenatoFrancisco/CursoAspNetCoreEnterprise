b3fec9cc1b8b5fcedc37f8b0b176f98522aa1501
renatofrancisco <renatofrancisco.s@gmail.com>
2023-02-02 20:51:03 -0300

    Definindo as entidades

---
+++ src/services/NSE.Cliente.API/Models/Address.cs
@@ -0,0 +1,33 @@
+﻿namespace NSE.Cliente.API.Models;
+
+public class Address : Entity
+{
+    public string Street { get; private set; }
+    public string Number { get; private set; }
+    public string Complement { get; private set; }
+    public string District { get; private set; }
+    public string ZipCode { get; private set; }
+    public string City { get; private set; }
+    public string State { get; private set; }
+
+    public Guid CustomerId { get; private set; }
+    public Customer Customer { get; protected set; }
+
+
+    public Address(string street, 
+                   string number, 
+                   string complement, 
+                   string district, 
+                   string zipCode, 
+                   string city, 
+                   string state)
+    {
+        Street = street;
+        Number = number;
+        Complement = complement;
+        District = district;
+        ZipCode = zipCode;
+        City = city;
+        State = state;
+    }
+}
---
+++ src/services/NSE.Cliente.API/Models/Customer.cs
@@ -0,0 +1,20 @@
+﻿namespace NSE.Cliente.API.Models;
+
+public class Customer : Entity, IAggregateRoot
+{
+    public string Name { get; private set; }
+    public string Email { get; private set; }
+    public string Cpf { get; private set; }
+    public bool Excluded { get; private set; }
+    public Address Address { get; private set; }
+
+    public Customer() { }
+
+    public Customer(string name, string email, string cpf)
+    {
+        Name = name;
+        Email = email;
+        Cpf = cpf;
+        Excluded = false;
+    }
+}
